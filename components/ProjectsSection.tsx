34cd3fd9633c8a123363d8b19f6e4d731dcbc2a0
Mambo <113513856+Mambo-ui@users.noreply.github.com>
2023-10-12 08:14:14 +0300

    ProjectsSection.tsx

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -26,7 +26,7 @@ const ProjectsSection = () => {
   return (
     <section id="projects">
       <h1 className="text-center font-bold text-4xl">
-        Projects
+        Projects [Not yet Done. Lol:)]
         <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
       </h1>
       <div className="flex flex-col space-y-28">
